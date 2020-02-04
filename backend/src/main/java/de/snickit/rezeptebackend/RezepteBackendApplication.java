bbfebdd6644575e59049b17a11a631b773e15d66
Dominik Kröll <dominik.kroell@mni.thm.de>
2020-02-04 01:27:55 +0100

    Initial commit

---
+++ backend/src/main/java/de/snickit/rezeptebackend/RezepteBackendApplication.java
@@ -0,0 +1,13 @@
+package de.snickit.rezeptebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezepteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezepteBackendApplication.class, args);
+	}
+
+}

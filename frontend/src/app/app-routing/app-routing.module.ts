7edafc6dc5cbeda7af2b14b543e856e43d82da21
Dominik Kröll <dominik.kroell@mni.thm.de>
2020-02-24 22:57:57 +0100

    Angular basic setup and experiments

---
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component'
+import { RecipeComponent } from '../recipe/recipe.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'recipe/:id',
+    component: RecipeComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
